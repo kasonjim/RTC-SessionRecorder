245b270bb96cf1c60598fbaa9786cc545fa2f5e3
Jason Kim <kasonjim@gmail.com>
2017-04-10 22:16:14 -0700

    Complete basic recording functionality (video only) with multi-user session page

--- lobby.js
+++ lobby.js
@@ -1,19 +1,119 @@
+///////////////////////////////////////////////////////////////
+//////////////////////   RECORDER Code   //////////////////////
+///////////////////////////////////////////////////////////////
+
+// Initial setup
+window.onbeforeunload = function() {
+  document.getElementById('start').disabled = false;
+  document.getElementById('stop').disabled = true;
+  document.getElementById('save').disabled = true;
+};
+
+// Get main recording element
+var elementToShare = document.getElementById('elementToShare');
+// Create canvas
+var canvas2d = document.createElement('canvas');
+var context = canvas2d.getContext('2d');
+canvas2d.width = elementToShare.clientWidth;
+canvas2d.height = elementToShare.clientHeight;
+canvas2d.style.top = 0;
+canvas2d.style.left = 0;
+canvas2d.style.zIndex = -1;
+(document.body || document.documentElement).appendChild(canvas2d);
+
+// State variables
+var isRecordingStarted = false;
+var isStoppedRecording = false;
+
+// Defining the videoRecorder instance and data storage variable
+var currentVideoBlob;
+var canvasRecorder = new CanvasRecorder(canvas2d, {
+  disableLogs: true
+});
+
+// Constantly checks state of recording/not-recording
+var looper = function() {
+  if (!isRecordingStarted) {
+    return setTimeout(looper, 500);
+  }
+  html2canvas(elementToShare, {
+    grabMouse: true,
+    onrendered: function(canvas) {
+      context.clearRect(0, 0, canvas2d.width, canvas2d.height);
+      context.drawImage(canvas, 0, 0, canvas2d.width, canvas2d.height);
+
+      if (isStoppedRecording) {
+        return;
+      }
+
+      setTimeout(looper, 1);
+    }
+  });
+};
+looper();
+
+// Button action for "START"
+document.getElementById('start').onclick = function() {
+  this.disabled = true;
+  document.getElementById('save').disabled = true;
+
+  // Set states
+  isStoppedRecording = false;
+  isRecordingStarted = true;
+  // Reset data
+  canvasRecorder.clearRecordedData();
+
+  // Start recording
+  canvasRecorder.record();
+
+  setTimeout(function() {
+    document.getElementById('stop').disabled = false;
+  }, 1000);
+};
+
+// Button action "STOP"
+document.getElementById('stop').onclick = function() {
+  this.disabled = true;
+  document.getElementById('start').disabled = false;
+
+  isStoppedRecording = true;
+  isRecordingStarted = false;
+
+  canvasRecorder.stop(function(blob) {
+    currentVideoBlob = blob;
+    document.getElementById('save').disabled = false;
+    looper();
+  });
+};
+
+// Button action for "SAVE"
+document.getElementById('save').onclick = function() {
+  console.log('VIDEO BLOB', currentVideoBlob);
+  convertStreams();
+};
+
+var convertStreams = function() {
+  var date = new Date();
+  var formatted = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()} (${date.getTime()})`;
+  invokeSaveAsDialog(currentVideoBlob, 'DC ' + formatted + '.webm');
+};
+
 /////////////////////////////////////////////////////////
 //////////////////////   UI Code   //////////////////////
 /////////////////////////////////////////////////////////
 document.getElementById('open-room').onclick = function() {
   disableInputButtons();
+  updateCloseLeaveButton();
 
-  connection.open(document.getElementById('room-id').value, function() {
+  var roomId = document.getElementById('room-id').value;
+
+  connection.open(roomId, function() {
     showRoomURL(connection.sessionid);
-    updateCloseLeaveButton();
 
     document.querySelector('#roomStatusText').innerHTML = '<h2>Waiting for participant(s) to join</h2>';
     document.getElementById('close-room').disabled = false;
 
-    var truth;
-    connection.isInitiator ? truth = 'YES' : truth = 'NO';
-    document.querySelector('#userRoleText').innerHTML = 'IS YOU THE ADMIN? ' + truth;
+    document.querySelector('#userRoleText').innerHTML = 'IS YOU THE ADMIN? ' + connection.isInitiator;
   });
 };
 
@@ -26,9 +126,7 @@ document.getElementById('join-room').onclick = function() {
   connection.checkPresence(roomId, function(isRoomExist, roomid) {
     if (isRoomExist) {
       connection.join(roomid, function() {
-        var truth;
-        connection.isInitiator ? truth = 'YES' : truth = 'NO';
-        document.querySelector('#userRoleText').innerHTML = 'IS YOU THE ADMIN? ' + truth;
+        document.querySelector('#userRoleText').innerHTML = 'IS YOU THE ADMIN? ' + connection.isInitiator;
       });
     } else {
       enableInputButtons();
@@ -60,7 +158,7 @@ var connection = new RTCMultiConnection();
 // comment-out below line if you do not have your own socket.io server
 // connection.socketURL = 'https://rtcmulticonnection.herokuapp.com:443/';
 connection.socketURL = 'http://localhost:443/';
-// connection.socketURL = 'http://idc-socket-server.herokuapp.com:443/';
+
 /*
     Cannot GET /socket.io/
       ?userid=64436r5sw4rts44wvg6a2
@@ -75,12 +173,10 @@ connection.socketURL = 'http://localhost:443/';
 
 // Initial connection setup
 connection.socketMessageEvent = 'interviewer.dc-room';
-connection.maxParticipantsAllowed = 2;
-connection.enableFileSharing = false; // by default, it is "false".
+connection.maxParticipantsAllowed = 3;
 connection.session = {
   audio: true,
-  video: true,
-  data: true
+  video: true
 };
 connection.sdpConstraints.mandatory = {
   OfferToReceiveAudio: true,
@@ -90,7 +186,7 @@ connection.sdpConstraints.mandatory = {
 // handling video sources
 connection.videosContainer = document.getElementById('videos-container');
 connection.onstream = function(event) {
-  var width = parseInt(connection.videosContainer.clientWidth / 3) - 20;
+  var width = parseInt(connection.videosContainer.clientWidth / 2) - 20;
   var mediaElement = getMediaElement(event.mediaElement, {
     title: event.userid,
     buttons: ['full-screen'],
@@ -112,7 +208,12 @@ connection.onstreamended = function(event) {
 
 // Handling session / rooms
 connection.onopen = function() {
+  if (!connection.isInitiator) {
+    document.getElementById('recordControls').style.display = 'none';
+  }
+
   updateCloseLeaveButton();
+
   document.getElementById('close-room').disabled = false;
   document.querySelector('#roomStatusText').innerHTML = 'You are connected with: ' + connection.getAllParticipants().join(', ');
 
@@ -175,7 +276,7 @@ var showRoomURL = function(roomid) {
   var roomQueryStringURL = '?roomid=' + roomid;
   var fullURL = window.location.href + roomQueryStringURL;
 
-  var html = '<h2>Unique URL for your room:</h2>';
+  var html = '<br /><strong>Room URL:</strong>';
   html += '<a href="' + roomQueryStringURL + '" target="_blank">' + fullURL + '</a>';
 
   document.getElementById('room-urls').innerHTML = html;
